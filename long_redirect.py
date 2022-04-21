28289f13ec675e12a5a739043fcc5fda97171920
Сухопаров Олег Александрович <OSukhoparov@alfabank.ru>
2022-04-21 12:50:50 +0300

    Some changes in readme and rename environment

--- long_redirect.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-payload = {"login": "secret_login", "password": "secret_pass"}
-
-
-res = requests.get("https://playground.learnqa.ru/api/long_redirect")
-redirects = res.history
-last_url = res.url
-print(len(redirects), last_url)
